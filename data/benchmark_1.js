bca8595b8f48084280a7837eecc20d8851993761
ShafaPricilla <75101698+ShafaPricilla@users.noreply.github.com>
2020-11-27 10:45:49 +0700

    Add files via upload

---
+++ data/benchmark_1.js
@@ -0,0 +1,8 @@
+var json_benchmark_1 = {
+"type": "FeatureCollection",
+"name": "benchmark_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.416478515830633, -7.007608596233469 ] } }
+]
+}
